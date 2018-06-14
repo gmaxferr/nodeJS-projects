6503bcf72280cd1b2ddbebf6636ae8274c4397c7
Guilherme Ferreira <gmaximianoferreira97@hotmail.com>
2018-06-14 17:30:54 +0100

    getting info from request (lat and lng)

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Praceta%20de%20Avilho%2079&key=AIzaSyDipPwo0vbJ6dnJag9KosAe_uOrLDcEGQY',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
